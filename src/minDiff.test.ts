a385d0bdc525d7028e243d44fe77adb7caceacf7
Joshua Pollak <abottomlesspit@gmail.com>
2021-06-04 00:55:03 -0700

    minDiff

---
+++ src/minDiff.test.ts
@@ -0,0 +1,75 @@
+import minDiff from "./minDiff";
+
+describe("minDiff", () => {
+    [
+        [
+            [undefined, undefined],
+            [undefined, undefined]
+        ],
+        [
+            ["apple", undefined],
+            ["a", undefined]
+        ],
+        [
+            ["apple", "butter"],
+            ["a", "b"]
+        ],
+        [
+            ["Apple", "apple"],
+            ["A", "a"]
+        ],
+        [
+            ["'apple'", "butter"],
+            ["a", "b"]
+        ],
+        [
+            ["Apple", "aqueous"],
+            ["Ap", "aq"]
+        ],
+        [
+            ["Apple", "apoplectic"],
+            ["App", "apo"]
+        ],
+        [
+            ["apiary", "apple"],
+            ["api", "app"]
+        ],
+        [
+            ["apple", undefined, { preA: "Acorn" }],
+            ["ap", undefined]
+        ],
+        [
+            ["apple", undefined, { preA: "Apse" }],
+            ["app", undefined]
+        ],
+        [
+            ["apple", undefined, { preA: "apple" }],
+            ["a", undefined]
+        ],
+        [
+            ["apple", undefined, { preA: "Apple" }],
+            ["a", undefined]
+        ],
+        [
+            ["apple", "butter", { preA: "Apse", postB: "buttress" }],
+            ["app", "butte"]
+        ],
+        [
+            ["apple", "butter", { preA: "Apse", postB: "butter" }],
+            ["app", "b"]
+        ],
+        [
+            ["Bloodrock", "Bobby Hutcherson", { preA: "Blood, Sweat", postB: "Bobby Hutcherson" }],
+            ["Bloodr", "Bo"]
+        ],
+    ].forEach(([[a, b, q], [c, d]]) => {
+        test(`given ${a} & ${b} & ${q && JSON.stringify(q)}, returns ${c}...${d}`, () => {
+            expect(minDiff(a, b, q)).toEqual([c, d]);
+        })
+    });
+
+    test("real-world examples", () => {
+        expect(minDiff("Bobby Hutcherson", "Brian Eno", { preA: "Bobby Hutcherson", postB: "Brian Eno" })).toEqual(["Bo", "Br"]);
+        expect(minDiff("Bloodrock", "Bobby Hutcherson", { preA: "Blood, Sweat And Tears", postB: "Bobby Hutcherson" })).toEqual(["Bloodr", "Bo"]);
+    })
+});
---
+++ src/minDiff.ts
@@ -0,0 +1,60 @@
+import zip from "lodash/zip";
+
+export default function minDiff(
+    a: string | undefined,
+    b: string | undefined,
+    { preA, postB }: {
+        preA?: string,
+        postB?: string
+    } = {}): [string | undefined, string | undefined] {
+    a = a?.replace(/\W/, "");
+    b = b?.replace(/\W/, "");
+    if (a === undefined) {
+        if (b === undefined) {
+            return [undefined, undefined];
+        }
+        if (postB !== undefined && !sameString(postB, b)) {
+            const [br,] = minDiff(b, postB);
+            return [undefined, br];
+        }
+        return [undefined, b[0]];
+    } else if (b === undefined) {
+        if (preA !== undefined && !sameString(preA, a)) {
+            const [, ar] = minDiff(preA, a);
+            return [ar, undefined];
+        }
+        return [a[0], undefined];
+    }
+    let aa = "";
+    let bb = "";
+    if (sameString(a, b)) {
+        aa = a[0];
+        bb = b[0];
+    } else {
+        const zipped = zip(a.split(""), b.split(""));
+        for (const [ca, cb] of zipped) {
+            if (ca) { aa += ca; }
+            if (cb) { bb += cb; }
+            if (ca !== cb && ca?.toLocaleLowerCase() !== cb?.toLocaleLowerCase()) {
+                break;
+            }
+        }
+    }
+    if (preA && !sameString(preA, a)) {
+        const [, pa] = minDiff(preA, a);
+        if (pa && pa.length > aa.length) {
+            aa = pa;
+        }
+    }
+    if (postB && !sameString(postB, b)) {
+        const [pb,] = minDiff(b, postB);
+        if (pb && pb.length > aa.length) {
+            bb = pb;
+        }
+    }
+    return [aa, bb];
+}
+function sameString(a: string, b: string) {
+    return a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase()) === 0;
+}
+
