3b2127ba7fa43ca3f2aad5d7cce797d3e358b0c1
Joshua Pollak <abottomlesspit@gmail.com>
2023-01-01 12:52:05 -0800

    fix required-ness

--- src/HTMLProps.ts
+++ src/HTMLProps.ts
@@ -1,8 +1,8 @@
 import { ClassNames } from "./classConcat";
 import type { HTMLAttributes } from "react";
 
-type HTMLProps = Partial<Pick<HTMLAttributes<"span">, "style">> & {
+type HTMLProps = Partial<Pick<HTMLAttributes<"span">, "style"> & {
     className: ClassNames,
-};
+}>;
 
 export default HTMLProps;
